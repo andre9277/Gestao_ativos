d1c4506a8cbb87962954b12693d942259629958c
Andre Ferreira <110822583+andre9277@users.noreply.github.com>
2023-03-23 17:54:20 +0000

    Pastas css frontend adicionadas ao projeto

--- asset_management/react/src/views/Login.jsx
+++ asset_management/react/src/views/Login.jsx
@@ -1,53 +1,106 @@
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 import axiosClient from "../axios-client.js";
-import {createRef} from "react";
-import {useStateContext} from "../context/ContextProvider.jsx";
+import { createRef } from "react";
+import { useStateContext } from "../context/ContextProvider.jsx";
 import { useState } from "react";
 
 export default function Login() {
-  const emailRef = createRef()
-  const passwordRef = createRef()
-  const { setUser, setToken } = useStateContext()
-  const [message, setMessage] = useState(null)
+  const emailRef = createRef();
+  const passwordRef = createRef();
+  const { setUser, setToken } = useStateContext();
+  const [message, setMessage] = useState(null);
 
-  const onSubmit = ev => {
-    ev.preventDefault()
+  const onSubmit = (ev) => {
+    ev.preventDefault();
 
     const payload = {
       email: emailRef.current.value,
       password: passwordRef.current.value,
-    }
-    axiosClient.post('/login', payload)
-      .then(({data}) => {
-        setUser(data.user)
+    };
+    axiosClient
+      .post("/login", payload)
+      .then(({ data }) => {
+        setUser(data.user);
         setToken(data.token);
       })
       .catch((err) => {
         const response = err.response;
         if (response && response.status === 422) {
-          setMessage(response.data.message)
+          setMessage(response.data.message);
         }
-      })
-  }
+      });
+  };
 
   return (
-    <div className="login-signup-form animated fadeInDown">
-      <div className="form">
-        <form onSubmit={onSubmit}>
-          <h1 className="title">Login into your account</h1>
+    <div id="layoutAuthentication">
+      <div id="layoutAuthentication_content">
+        <main>
+          <div className="container">
+            <div className="row justify-content-center">
+              <div className="col-lg-5">
+                <div className="card shadow-lg border-0 rounded-lg mt-5">
+                  <div className="card-header">
+                    <h3 className="text-center font-weight-light my-4">
+                      Login
+                    </h3>
+                  </div>
 
-          {message &&
-            <div className="alert">
-              <p>{message}</p>
+                  <div className="form">
+                    <form onSubmit={onSubmit}>
+                      {message && (
+                        <div className="alert">
+                          <p>{message}</p>
+                        </div>
+                      )}
+                      <div className="form-floating mb-3">
+                        <label htmlFor="inputEmail">Email address</label>
+                        <input
+                          ref={emailRef}
+                          className="form-control"
+                          id="inputEmail"
+                          type="email"
+                          placeholder="name@example.com"
+                        />
+                      </div>
+                      <div className="form-floating mb-3">
+                        <label htmlFor="inputPassword">Password</label>
+                        <input
+                          ref={passwordRef}
+                          className="form-control"
+                          id="inputPassword"
+                          type="password"
+                          placeholder="Password"
+                          autoComplete="false"
+                        />
+                      </div>
+                      <div className="form-check mb-3">
+                        <input
+                          className="form-check-input"
+                          id="inputRememberPassword"
+                          type="checkbox"
+                          value=""
+                        />
+                        <label
+                          className="form-check-label"
+                          htmlFor="inputRememberPassword"
+                        >
+                          Remember Password
+                        </label>
+                      </div>
+                      <div className="d-flex align-items-center justify-content-between mt-4 mb-0">
+                        <Link to="/resetpass" className="small">
+                          Forgot Password?
+                        </Link>
+                        <button className="btn btn-block">Login</button>
+                      </div>
+                    </form>
+                  </div>
+                </div>
+              </div>
             </div>
-          }
-
-          <input ref={emailRef} type="email" placeholder="Email"/>
-          <input ref={passwordRef} type="password" placeholder="Password"/>
-          <button className="btn btn-block">Login</button>
-          <p className="message">Not registered? <Link to="/signup">Create an account</Link></p>
-        </form>
+          </div>
+        </main>
       </div>
     </div>
-  )
+  );
 }
