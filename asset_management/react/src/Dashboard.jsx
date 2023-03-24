2b732d1f00aa38104a2b8936069df2f69542e1ff
Andre Ferreira <110822583+andre9277@users.noreply.github.com>
2023-03-24 11:30:03 +0000

    Algum Frontend Implementado
    Implementado frontend, nomeadamente  o Dashboard, TopBar, Sidebar e Footer

--- asset_management/react/src/Dashboard.jsx
+++ asset_management/react/src/Dashboard.jsx
@@ -1,12 +1,96 @@
-import { useState } from 'react'
+import { useState } from "react";
+import Card from "./components/Card.jsx";
+import AreaChart from "./components/AreaChart";
+import PieChart from "./components/PieChart";
+import ProjectCard from "./components/ProjectCard";
+import Illustration from "./components/Illustration";
+import Approach from "./components/Approach";
 
 function Dashboard() {
-
   return (
-    <div>
-        <h1>Dashboard</h1>
-    </div>
-  )
+    <>
+      {/*  <!-- Main Content --> */}
+      <div id="content">
+        {/* <!-- Begin Page Content --> */}
+        <div className="container-fluid">
+          {/*  <!-- Page Heading --> */}
+          <div className="d-sm-flex align-items-center justify-content-between mb-4">
+            <h1 className="h3 mb-0 text-gray-800">Dashboard</h1>
+          </div>
+
+          {/*  <!-- Content Row --> */}
+          <div className="row">
+            {/*  <!-- Total de Ativos --> */}
+            <Card
+              Titulo="Total de ativos"
+              Descricao="1295"
+              Icon="fa-laptop"
+              Cor="text-primary"
+              Tipo="primary"
+            />
+
+            {/*  <!-- Ativos Alterados --> */}
+            <Card
+              Titulo="Ativos Alterados (Mensalmente)"
+              Descricao="83"
+              Icon="fa-arrows-alt"
+              Cor="text-success"
+              Tipo="success"
+            />
+
+            {/*  <!-- Ativos em Estado Inativo --> */}
+
+            <Card
+              Titulo="Ativos em Estado Inativo"
+              Descricao="10%"
+              Icon="fa-lock"
+              Cor="text-info"
+              Tipo="info"
+            />
+
+            {/*  <!-- Ativos em Reparação --> */}
+
+            <Card
+              Titulo="Ativos em Reparação"
+              Descricao="27"
+              Icon="fa-wrench"
+              Cor="text-warning"
+              Tipo="warning"
+            />
+          </div>
+
+          {/*  <!-- Content Row --> */}
+
+          <div className="row">
+            {/*   <!-- Area Chart --> */}
+            <AreaChart />
+
+            {/*  <!-- Pie Chart --> */}
+            <PieChart />
+          </div>
+
+          {/*   <!-- Content Row --> */}
+          <div className="row">
+            {/*   <!-- Content Column --> */}
+            <div className="col-lg-6 mb-4">
+              {/* <!-- Project Card Example --> */}
+              <ProjectCard />
+            </div>
+
+            <div className="col-lg-6 mb-4">
+              {/* <!-- Illustrations --> */}
+              <Illustration />
+
+              {/* <!-- Approach --> */}
+              <Approach />
+            </div>
+          </div>
+        </div>
+        {/*   <!-- /.container-fluid --> */}
+      </div>
+      {/*   <!-- End of Main Content -->*/}
+    </>
+  );
 }
 
-export default Dashboard
+export default Dashboard;
--- asset_management/react/src/components/DefaultLayout.jsx
+++ asset_management/react/src/components/DefaultLayout.jsx
@@ -1,58 +1,49 @@
-import {Link, Navigate, Outlet} from "react-router-dom";
-import {useStateContext} from "../context/ContextProvider";
+import { Link, Navigate, Outlet } from "react-router-dom";
+import { useStateContext } from "../context/ContextProvider";
 import axiosClient from "../axios-client.js";
-import {useEffect} from "react";
+import { useEffect } from "react";
+import SideBar from "./SideBar";
+import TopBar from "./TopBar";
+import Footer from "./Footer";
 
 export default function DefaultLayout() {
-  const {user, token, setUser, setToken, notification} = useStateContext();
+  const { token, setUser, setToken, notification } = useStateContext();
 
   if (!token) {
-    return <Navigate to="/login"/>
+    return <Navigate to="/login" />;
   }
 
-  const onLogout = ev => {
-    ev.preventDefault()
+  const onLogout = (ev) => {
+    ev.preventDefault();
 
-    axiosClient.post('/logout')
-      .then(() => {
-        setUser({})
-        setToken(null)
-      })
-  }
+    axiosClient.post("/logout").then(() => {
+      setUser({});
+      setToken(null);
+    });
+  };
 
   useEffect(() => {
-    axiosClient.get('/user')
-      .then(({data}) => {
-         setUser(data)
-      })
-  }, [])
+    axiosClient.get("/user").then(({ data }) => {
+      setUser(data);
+    });
+  }, []);
 
   return (
-    <div id="defaultLayout">
-      <aside>
-        <Link to="/dashboard">Dashboard</Link>
-        <Link to="/users">Users</Link>
-      </aside>
-      <div className="content">
-        <header>
-          <div>
-            Header
-          </div>
+    <div id="wrapper">
+      <div id="defaultLayout">
+        <SideBar />
 
-          <div>
-            {user.name} &nbsp; &nbsp;
-            <a onClick={onLogout} className="btn-logout" href="#">Logout</a>
+        <div id="content-wrapper" className="d-flex flex-column">
+          <div className="content">
+            <TopBar onLogout={onLogout} />
+            <main>
+              <Outlet />
+            </main>
+            {notification && <div className="notification">{notification}</div>}
           </div>
-        </header>
-        <main>
-          <Outlet/>
-        </main>
-        {notification &&
-          <div className="notification">
-            {notification}
-          </div>
-        }
+          <Footer />
+        </div>
       </div>
     </div>
-  )
+  );
 }
