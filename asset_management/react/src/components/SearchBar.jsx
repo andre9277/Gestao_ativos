d75adc68c571327fcd0e1a3f55c2a4402abd86ad
Andre Ferreira <110822583+andre9277@users.noreply.github.com>
2023-06-09 09:29:47 +0100

    Procura binária na Pesquisa de ativos
    Implementada a procura binária na procura de ativos, é possível procurar por número de série e número de inventário. Caso não encontre uma correspondência o utilizador é redirecionado para a página não encontrada.

--- asset_management/react/src/components/SearchBar.jsx
+++ asset_management/react/src/components/SearchBar.jsx
@@ -27,7 +27,7 @@ You may obtain a copy of the license at:
 
 All the changes made to enable the implementation of the desired development tools were made by André Ferreira.
 */
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import axiosClient from "../axios-client.js";
 import "../styles/SearchBar.css";
 import { useNavigate } from "react-router-dom";
@@ -37,34 +37,64 @@ const Search = () => {
   const navigate = useNavigate();
 
   const [assetNumber, setAssetNumber] = useState("");
+  const [assets, setAssets] = useState([]);
+
+  useEffect(() => {
+    getAssets();
+  }, []);
+
+  //Performs a client access request
+  const getAssets = (url) => {
+    url = url || "/allAssets";
+
+    axiosClient.get(url).then(({ data }) => {
+      setAssets(data.data);
+    });
+  };
 
   const handleChange = (event) => {
     setAssetNumber(event.target.value);
   };
 
-  const handleSubmit = async (event) => {
-    event.preventDefault();
-    if (assetNumber.trim() !== "") {
-      try {
-        const response = await axiosClient.get(
-          `/assetSearch?numb_inv=${assetNumber}`
-        );
+  //Binary Search to improve performance in search
+  const binarySearch = (arr, target) => {
+    let left = 0;
+    let right = arr.length - 1;
 
-        const responseSer = await axiosClient.get(
-          `/assetSearchSer?numb_ser=${assetNumber}`
-        );
+    while (left <= right) {
+      const mid = Math.floor((left + right) / 2);
 
-        if (responseSer.data.length === 0) {
-          const assetId = response.data[0].id; // Assuming the response contains the asset ID
-          navigate(`/infoasset/${assetId}`);
-        } else {
-          const assetId = responseSer.data[0].id;
-          navigate(`/infoasset/${assetId}`);
-        }
-      } catch (error) {
-        navigate("/*");
+      if (arr[mid].numb_inv === target || arr[mid].numb_ser === target) {
+        return arr[mid];
+      } else if (arr[mid].numb_inv < target || arr[mid].numb_ser < target) {
+        left = mid + 1;
+      } else {
+        right = mid - 1;
       }
     }
+
+    return null;
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(assets);
+    // Sort the assets array by asset number (assuming it's not already sorted)
+    const sortedAssets = [...assets].sort(
+      (a, b) =>
+        a.numb_inv.localeCompare(b.numb_inv) ||
+        a.numb_ser.localeCompare(b.numb_ser)
+    );
+
+    // Search for the asset by asset number or serial number using binary search
+    const matchedAsset = binarySearch(sortedAssets, assetNumber);
+
+    if (matchedAsset) {
+      navigate(`/infoasset/${matchedAsset.id}`);
+    } else {
+      navigate("/*");
+    }
+
     setAssetNumber("");
   };
 
