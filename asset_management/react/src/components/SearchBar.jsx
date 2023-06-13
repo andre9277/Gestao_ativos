c5b7ef5dd104d4fba221f50bfe02b67abb00996c
Andre Ferreira <110822583+andre9277@users.noreply.github.com>
2023-06-13 11:29:59 +0100

    SearchBar c/ número de série

--- asset_management/react/src/components/SearchBar.jsx
+++ asset_management/react/src/components/SearchBar.jsx
@@ -58,15 +58,25 @@ const Search = () => {
 
   //Binary Search to improve performance in search
   const binarySearch = (arr, target) => {
-    let left = 0;
+    if (arr[0].numb_ser === target || arr[0].numb_inv === target) {
+      return arr[0];
+    }
+
+    let left = 1;
     let right = arr.length - 1;
 
     while (left <= right) {
       const mid = Math.floor((left + right) / 2);
+      const currentAsset = arr[mid];
 
-      if (arr[mid].numb_inv === target || arr[mid].numb_ser === target) {
-        return arr[mid];
-      } else if (arr[mid].numb_inv < target || arr[mid].numb_ser < target) {
+      if (
+        currentAsset.numb_ser === target ||
+        currentAsset.numb_inv === target
+      ) {
+        return currentAsset;
+      }
+
+      if (currentAsset.numb_ser < target) {
         left = mid + 1;
       } else {
         right = mid - 1;
@@ -80,15 +90,22 @@ const Search = () => {
     event.preventDefault();
 
     // Sort the assets array by asset number (assuming it's not already sorted)
-    const sortedAssets = [...assets].sort(
-      (a, b) =>
-        a.numb_inv.localeCompare(b.numb_inv) ||
-        a.numb_ser.localeCompare(b.numb_ser)
-    );
+    const sortedAssets = [...assets].sort((a, b) => {
+      if (a.numb_ser === null && b.numb_ser === null) {
+        return a.id.localeCompare(b.id); // Sort by ID if both ser are null
+      } else if (a.numb_ser === null) {
+        return 1; // Put null ser at the end
+      } else if (b.numb_ser === null) {
+        return -1; // Put null ser at the end
+      } else {
+        return a.numb_ser.localeCompare(b.numb_ser);
+      }
+    });
 
     // Search for the asset by asset number or serial number using binary search
     const matchedAsset = binarySearch(sortedAssets, assetNumber);
 
+    console.log(matchedAsset);
     if (matchedAsset) {
       navigate(`/infoasset/${matchedAsset.id}`);
     } else {
