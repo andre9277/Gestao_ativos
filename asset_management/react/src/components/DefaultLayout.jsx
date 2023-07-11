57744c2aa82676392212182b96834a7b622c7cdd
Andre Ferreira <110822583+andre9277@users.noreply.github.com>
2023-07-11 17:31:37 +0100

    Headers assinatura - web

--- asset_management/react/src/components/DefaultLayout.jsx
+++ asset_management/react/src/components/DefaultLayout.jsx
@@ -25,6 +25,12 @@ You may obtain a copy of the license at:
       https://github.com/StartBootstrap/startbootstrap-sb-admin-2
 
 
+Project developed under the EstágiAP XXI Program.
+Advisor: Emanuel Gonçalves
+Autor: André Ferreira
+Local: Hospital de Braga, EPE
+Department: Serviço de Sistema de Informação
+
 All the changes made to enable the implementation of the desired development tools were made by André Ferreira.
 */
 import { Navigate, Outlet } from "react-router-dom";
