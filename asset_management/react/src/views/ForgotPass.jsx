7b9e424fc3a0bb2d3f39eb965081e724a2fa5049
Andre Ferreira <110822583+andre9277@users.noreply.github.com>
2023-07-19 14:56:33 +0100

    Update Forgot password dashboard

--- asset_management/react/src/views/ForgotPass.jsx
+++ asset_management/react/src/views/ForgotPass.jsx
@@ -70,7 +70,7 @@ const ForgotPass = () => {
           <div className="container">
             <div className="justify-content-center">
               <div className="col-lg-5">
-                <div className="card shadow-lg border-0 rounded-lg mt-5">
+                <div className="carddd shadow-lg border-0 rounded-lg mt-5">
                   <div className="card-header">
                     <h3 className="text-center font-weight-light my-4">
                       Recuperação da Password
