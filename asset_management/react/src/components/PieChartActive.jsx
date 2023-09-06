5fa84c5954c82222ab15b2cfc3f2aa91e211a436
Andre Ferreira <110822583+andre9277@users.noreply.github.com>
2023-09-06 17:36:49 +0100

    Update PieChartActive.jsx

--- asset_management/react/src/components/PieChartActive.jsx
+++ asset_management/react/src/components/PieChartActive.jsx
@@ -37,6 +37,7 @@ import React from "react";
 import { Pie } from "react-chartjs-2";
 import { Chart as ChartJS, Tooltip, Legend, ArcElement } from "chart.js";
 
+//registers the chartjs
 ChartJS.register(Tooltip, Legend, ArcElement);
 
 const PieChartActive = ({ assets }) => {
